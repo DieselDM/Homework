f22970d11b3477e1dd98bf2f56c8e0aa19ab79af
DieselDM <david.hartman@g.austincc.edu>
2016-07-07 19:36:35 -0500

    HW#1 main

---
+++ HW1_main.cpp
@@ -0,0 +1,24 @@
+/* David Hartman
+COSC-2425
+Professor Roie Black
+
+HW #1 "Hello World" 
+
+This is main file which will include the helloWorld.cpp. 
+The helloWorld.ccp will be called to display the message.*/
+
+#include <string>
+#include <iostream>
+#include "helloWorld.cpp"
+
+using namespace std;
+
+int main()
+{
+	helloWorld();
+	return 0;
+}
+
+
+
+
