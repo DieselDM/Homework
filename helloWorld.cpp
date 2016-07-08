2599b3454d43f614aa7e774f9a7bbe2f70bc94a6
DieselDM <david.hartman@g.austincc.edu>
2016-07-07 19:37:25 -0500

    HW#1 Hello World display function program

---
+++ helloWorld.cpp
@@ -0,0 +1,17 @@
+/* David Hartman
+COSC-2425
+Professor Roie Black
+
+HW #1 "Hello World" 
+
+The helloWorld.ccp will be called to display the message to main.*/
+
+#include <iostream>
+
+using namespace std;
+
+void helloWorld()
+{
+	cout << "Hello World" << endl;
+	
+}
